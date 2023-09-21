7654082e64db23565e59cf198448e5fb6eec4de6
Kirimochi-jx2 <s2222035@stu.musashino-u.ac.jp>
2023-09-21 20:19:03 +0900

    add

--- playwright/station01.spec.ts
+++ playwright/station01.spec.ts
@@ -1,26 +1,46 @@
 
 // 「リストを作ろう！」 - HTML List
+//りんご・ばなな・ぶどうをリスト表示できている。
+// 1. <ul>が存在する
+// 2. <li>で囲われた「りんご」、「ばなな」、「ぶどう」がそれぞれ表示されている
 
+import { test, expect } from "@playwright/test";
 
-import { test, expect } from '@playwright/test' 
+test("リストを作ろう！", async ({ page }) => { // 1. <ul>が存在する
+  await page.goto("https://playwright.dev/");
+  const list = await page.$("ul");
+  expect(list).toBeTruthy();
+}
+);
 
-test.beforeEach(async ({ page }) => {
-  await page.goto('/station1.html')
-})
+test("リストを作ろう！", async ({ page }) => { // 2. <li>で囲われた「りんご」、「ばなな」、「ぶどう」がそれぞれ表示されている
+  await page.goto("https://playwright.dev/");
+  const list = await page.$$("li");
+  expect(list[0]).toHaveText("りんご");
+  expect(list[1]).toHaveText("ばなな");
+  expect(list[2]).toHaveText("ぶどう");
+}
+);
 
-test('<ul>が存在する', async ({ page }) => {
-  await expect(page).toHaveSelector('ul')
-})
+// import { test, expect } from '@playwright/test'  // testとexpectをインポート
 
-test('<li>で囲われた「りんご」、「ばなな」、「ぶどう」がそれぞれ表示されている', async ({
-  page,
-}) => {
-  // ul > li
-  const lis = await page.$$('ul > li')
-  const names = ['りんご', 'ばなな', 'ぶどう']
-  const liTexts = await Promise.all(lis.map(li => li.textContent()))
+// test.beforeEach(async ({ page }) => { // テストの前に実行する処理
+//   await page.goto('/station1.html') // テスト対象のページに移動する
+// })
 
-  names.forEach(name => {
-    expect(liTexts).toContain(name)
-  })
-})
+// test('<ul>が存在する', async ({ page }) => { // テストの実行
+//   await expect(page).toHaveSelector('ul') // expectはテストの期待値を設定する
+// }) // toHaveSelectorは指定したセレクタが存在するかを確認する
+
+// test('<li>で囲われた「りんご」、「ばなな」、「ぶどう」がそれぞれ表示されている', async ({ // テストの実行
+//   page, // pageはブラウザのページ
+// }) => { 
+//   // ul > li は ulの子要素のliを取得するセレクタ
+//   const lis = await page.$$('ul > li') // $$は複数の要素を取得する
+//   const names = ['りんご', 'ばなな', 'ぶどう'] // 期待する名前
+//   const liTexts = await Promise.all(lis.map(li => li.textContent())) // mapは配列の要素を変換する
+
+//   names.forEach(name => { // forEachは配列の要素を順番に処理する
+//     expect(liTexts).toContain(name) // expectはテストの期待値を設定する
+//   })
+// })
