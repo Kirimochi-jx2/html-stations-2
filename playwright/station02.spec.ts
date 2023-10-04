e010b94fe4a392f1c1a693dbcf7235f982191e46
Kirimochi-jx2 <s2222035@stu.musashino-u.ac.jp>
2023-10-04 18:08:55 +0900

    add

--- playwright/station02.spec.ts
+++ playwright/station02.spec.ts
@@ -1,52 +1,27 @@
-// import { expect, test } from '@playwright/test'
+import { expect, test } from '@playwright/test'
 
 
-// //  floatを使ってレイアウトを作ろう！」 - floatを使ってレイアウトを作ろう！ | CodeGrid
+//  floatを使ってレイアウトを作ろう！」 - floatを使ってレイアウトを作ろう！ | CodeGrid
 
-// test.beforeEach(async ({ page }) => { // ❶
-//   await page.goto('/station2.html') // ❷
-// })
+test.beforeEach(async ({ page }) => { // ❶
+  await page.goto('/station2.html') // ❷
+})
 
-// test('#redと#blueが横並びになっている', async ({ page }) => { // ❸
-//   const redFloat = await page.$eval('#red', el => getComputedStyle(el).float) // const redFloatは、#redのfloatの値を取得している
-//   const blueFloat = await page.$eval('#blue', el => getComputedStyle(el).float)　// const blueFloatは、#blueのfloatの値を取得している
+test('#redと#blueが横並びになっている', async ({ page }) => { // ❸
+  const redFloat = await page.$eval('#red', el => getComputedStyle(el).float) // const redFloatは、#redのfloatの値を取得している
+  const blueFloat = await page.$eval('#blue', el => getComputedStyle(el).float)　// const blueFloatは、#blueのfloatの値を取得している
 
-//   expect(redFloat).toBe('left')　// redFloatは、leftであることを期待している
-//   expect(blueFloat).toBe('left')　// blueFloatは、leftであることを期待している
-// })
-
-// test('横並びになっている#redと#blueの下に#greenが表示されている', async ({
-//   page,
-// }) => {
-//   const greenClear = await page.$eval( // greenClearは、#greenのclearの値を取得している
-//     '#green',
-//     el => getComputedStyle(el).clear, // clearは、要素の両側に浮動要素を許可しないことを指定するプロパティ
-//   )
-
-//   expect(greenClear).toBe('both') // greenClearは、bothであることを期待している
-// })
-
-import { expect, test } from '@playwright/test';
-
-test.beforeEach(async ({ page }) => {
-  await page.goto('/station2.html');
-});
-
-test('#redと#blueが横並びになっている', async ({ page }) => {
-  const redFloat = await page.$eval('#red', el => getComputedStyle(el).float);
-  const blueFloat = await page.$eval('#blue', el => getComputedStyle(el).float);
-
-  expect(redFloat).toBe('left');
-  expect(blueFloat).toBe('left');
-});
+  expect(redFloat).toBe('left')　// redFloatは、leftであることを期待している
+  expect(blueFloat).toBe('left')　// blueFloatは、leftであることを期待している
+})
 
 test('横並びになっている#redと#blueの下に#greenが表示されている', async ({
   page,
 }) => {
-  const greenClear = await page.$eval(
+  const greenClear = await page.$eval( // greenClearは、#greenのclearの値を取得している
     '#green',
-    el => getComputedStyle(el).clear,
-  );
+    el => getComputedStyle(el).clear, // clearは、要素の両側に浮動要素を許可しないことを指定するプロパティ
+  )
 
-  expect(greenClear).toBe('both');
-});
+  expect(greenClear).toBe('both') // greenClearは、bothであることを期待している
+})
